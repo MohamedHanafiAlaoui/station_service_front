aca32be404090b10ab8de40358500ba088751606
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2026-03-22 19:56:00 +0100

    feat: Implement admin pump management, employee sales, station details, and comprehensive NGRX state management for the station service application.

--- src/app/features/employe/station-details/station-details.ts
+++ src/app/features/employe/station-details/station-details.ts
@@ -5,9 +5,7 @@ import { Observable } from 'rxjs';
 import { Station } from '../../../core/models/station';
 import * as StationActions from '../store/station/station.actions';
 import { selectCurrentStation, selectStationLoading, selectStationError } from '../store/station/station.selectors';
-
 import { LoadingSpinnerComponent } from '../../../shared/components/loading-spinner/loading-spinner.component';
-
 @Component({
   selector: 'app-station-details',
   standalone: true,
@@ -19,16 +17,12 @@ export class StationDetails implements OnInit {
   station$: Observable<Station | null>;
   loading$: Observable<boolean>;
   error$: Observable<string | null>;
-
-  // Harcoded for mockup
-  stationId = 1;
-
+  stationId = 2;
   constructor(private store: Store) {
     this.station$ = this.store.select(selectCurrentStation);
     this.loading$ = this.store.select(selectStationLoading);
     this.error$ = this.store.select(selectStationError);
   }
-
   ngOnInit(): void {
     this.store.dispatch(StationActions.loadStation({ id: this.stationId }));
   }
