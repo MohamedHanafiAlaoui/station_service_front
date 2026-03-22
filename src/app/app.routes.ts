712882cf79bec9065d9eb51eec6fca23b11a71fa
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2026-03-22 15:42:37 +0100

    feat: Initialize project with core structure, authentication, and distinct admin, employe, and client modules.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,54 +1,42 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './core/guards/auth-guard';
 import { loginRedirectGuard } from './core/guards/login-redirect-guard';
-
 export const routes: Routes = [
-
   {
     path: 'auth',
     canActivate: [loginRedirectGuard],
     loadChildren: () =>
       import('./features/auth/auth.routes').then(m => m.AUTH_ROUTES),
   },
-
   {
     path: 'admin',
     canActivate: [authGuard],
     data: { role: 'ROLE_ADMIN' },
-
     loadChildren: () =>
       import('./features/admin/admin.routes').then(m => m.ADMIN_ROUTES),
-
     loadComponent: () =>
       import('./features/admin/admin-layout/adminlayout/adminlayout')
         .then(m => m.Adminlayout),
   },
-
   {
     path: 'employe',
     canActivate: [authGuard],
     data: { role: 'ROLE_EMPLOYE' },
-
     loadChildren: () =>
       import('./features/employe/employe.routes').then(m => m.EMPLOYE_ROUTES),
-
     loadComponent: () =>
       import('./shared/components/layout/layout')
         .then(m => m.Layout),
   },
-
   {
     path: 'client',
     canActivate: [authGuard],
     data: { role: 'ROLE_CLIENT' },
-
     loadChildren: () =>
       import('./features/client/client.routes').then(m => m.CLIENT_ROUTES),
-
     loadComponent: () =>
       import('./shared/components/layout/layout')
         .then(m => m.Layout),
   },
-
   { path: '', redirectTo: 'auth', pathMatch: 'full' }
 ];
--- src/app/core/api/api.ts
+++ src/app/core/api/api.ts
@@ -1,27 +1,18 @@
 export class Api {
   public static readonly BASE = 'http://localhost:8080/api';
-
   public static readonly AUTH = `${Api.BASE}/auth`;
   public static readonly LOGIN = `${Api.AUTH}/login`;
   public static readonly REGISTER = `${Api.AUTH}/register`;
   public static readonly REGISTER_EMPLOYE = `${Api.AUTH}/register/Employe`;
   public static readonly EMPLOYES = `${Api.AUTH}/employes`;
-
   public static readonly USERS = `${Api.BASE}/users`;
   public static readonly UPDATE_NAME = (id: number) => `${Api.AUTH}/update/${id}`;
-
   public static readonly CLIENTS = `${Api.BASE}/clients`;
-
   public static readonly STATIONS = `${Api.BASE}/stations`;
   public static readonly PUBLIC_STATIONS = `${Api.STATIONS}/public`;
-
   public static readonly POMPES = `${Api.BASE}/pompes`;
-
   public static readonly VENTES = `${Api.BASE}/ventes`;
-
   public static readonly APPROVISIONNEMENTS = `${Api.BASE}/approvisionnements`;
-
   public static readonly JOURNALS = `${Api.BASE}/journals`;
-  
   public static readonly BADGE = `${Api.BASE}/badge`;
 }
