7d6d223dee6eba4b32c21e84c315ff572a3c5e5b
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2026-03-22 18:00:23 +0100

    feat: Introduce comprehensive client, employee, and admin modules, including state management and shared UI components.

--- src/app/features/auth/register/register.ts
+++ src/app/features/auth/register/register.ts
@@ -4,7 +4,6 @@ import { Router, RouterModule } from '@angular/router';
 import { RegisterRequest } from '../../../core/models/register-request';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
 @Component({
   selector: 'app-register',
   standalone: true,
@@ -13,7 +12,6 @@ import { CommonModule } from '@angular/common';
   styleUrl: './register.css',
 })
 export class Register {
-
   form: RegisterRequest = {
     username: '',
     nom: '',
@@ -21,42 +19,30 @@ export class Register {
     password: '',
     role: 'CLIENT'
   };
-
   error = '';
   success = '';
-
-  // FRONT-END REGEX (نفس backend)
   USERNAME_REGEX = /^[a-zA-Z0-9._-]{4,20}$/;
   NAME_REGEX = /^[a-zA-ZÀ-ÿ\s'-]{2,50}$/;
   PASSWORD_REGEX = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$%^&+=!]).{8,}$/;
-
   constructor(private auth: AuthService, private router: Router) {}
-
   register() {
-
-    // FRONT-END VALIDATION
     if (!this.form.username || !this.USERNAME_REGEX.test(this.form.username)) {
       this.error = "Nom d'utilisateur invalide (4–20 caractères, lettres/chiffres uniquement)";
       return;
     }
-
     if (!this.form.nom || !this.NAME_REGEX.test(this.form.nom)) {
       this.error = "Nom invalide (2–50 caractères)";
       return;
     }
-
     if (!this.form.prenom || !this.NAME_REGEX.test(this.form.prenom)) {
       this.error = "Prénom invalide (2–50 caractères)";
       return;
     }
-
     if (!this.form.password || !this.PASSWORD_REGEX.test(this.form.password)) {
       this.error = "Mot de passe invalide (8+ caractères, majuscule, minuscule, chiffre, symbole)";
       return;
     }
-
     this.error = '';
-
     this.auth.register(this.form).subscribe({
       next: () => {
         this.success = 'Compte créé avec succès !';
