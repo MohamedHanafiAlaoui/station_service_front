985f76d5d1a72914e1e40c218adf7d096e4a8a87
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2026-03-22 19:19:58 +0100

    feat: Implement core features for station administration, employee operations, and client functionalities.

--- src/app/features/client/clientlayout/clientlayout.ts
+++ src/app/features/client/clientlayout/clientlayout.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ClientNavbar } from "../client-navbar/client-navbar";
-import { RouterModule } from "@angular/router";
-
+import { RouterModule, Router, NavigationEnd } from "@angular/router";
+import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs/operators';
 @Component({
   selector: 'app-clientlayout',
-  imports: [ ClientNavbar, RouterModule],
+  standalone: true,
+  imports: [ ClientNavbar, RouterModule, CommonModule],
   templateUrl: './clientlayout.html',
   styleUrl: './clientlayout.css',
 })
-export class Clientlayout {}
+export class Clientlayout implements OnInit {
+  isSidebarOpen = false;
+  private readonly router = inject(Router);
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => this.isSidebarOpen = false);
+  }
+  toggleSidebar(): void {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
+  closeSidebar(): void {
+    this.isSidebarOpen = false;
+  }
+}
