85668677e4166b43b22ee43e330c1718be54b7ca
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2026-03-22 18:00:50 +0100

    feat: Implement comprehensive client and employee modules with NGRX state management and shared UI components.

--- src/app/features/client/client.routes.ts
+++ src/app/features/client/client.routes.ts
@@ -1,15 +1,11 @@
 import { Routes } from '@angular/router';
 import { provideEffects } from '@ngrx/effects';
 import { provideState } from '@ngrx/store';
-
 import { Historique } from './historique/historique';
 import { Profil } from './profil/profil';
-import { Solde } from './solde/solde';
 import { Dashboard } from './dashboard/dashboard';
-
 import { ClientEffects } from './store/client.effects';
 import { clientReducer } from './store/client.reducer';
-
 export const CLIENT_ROUTES: Routes = [
   {
     path: '',
@@ -19,7 +15,6 @@ export const CLIENT_ROUTES: Routes = [
     ],
     children: [
       { path: '', component: Dashboard },
-      { path: 'solde', component: Solde },
       { path: 'historique', component: Historique },
       { path: 'profil', component: Profil }
     ]
