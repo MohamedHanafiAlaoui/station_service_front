b1354d50eb52a717eeca174c7bbd360e64419311
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2026-03-23 12:13:20 +0100

    feat: Introduce new shared UI components including sidebar, table, layout, station map, navbar, modal, and loading spinner.

--- src/app/shared/components/layout/layout.ts
+++ src/app/shared/components/layout/layout.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { Sidebar } from '../sidebar/sidebar';
 import { Navbar } from '../navbar/navbar';
 import { RouterOutlet } from '@angular/router';
-
-
 @Component({
   selector: 'app-layout',
-  imports: [ Navbar, RouterOutlet],
+  standalone: true,
+  imports: [Sidebar, Navbar, RouterOutlet],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
