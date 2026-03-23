4666444578f0bb3c22ea756571b8db0fcb187609
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2026-03-23 12:15:48 +0100

    feat: add shared Navbar, Sidebar, StationMap, Table, and Toast components.

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,15 +1,10 @@
 import { Component, inject } from '@angular/core';
-import {  AsyncPipe } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 import { AuthService } from '../../../core/services/Auth';
-import { ClientNavbar } from '../../../features/client/client-navbar/client-navbar';
-import { EmployeNavbar } from '../../../features/employe/employe-navbar/employe-navbar';
-import { AdminNavbar } from '../../../features/admin/admin-navbar/admin-navbar';
-
-
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [ AsyncPipe, ClientNavbar, EmployeNavbar, AdminNavbar],
+  imports: [ AsyncPipe ],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
