6fd2422768df825cbd2dae32202d5392dae01970
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2026-03-23 12:16:42 +0100

    feat: add new shared UI components for station map, generic table, toast notifications, and sidebar.

--- src/app/shared/components/sidebar/sidebar.ts
+++ src/app/shared/components/sidebar/sidebar.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
