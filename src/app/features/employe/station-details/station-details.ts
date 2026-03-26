7ebc05bbfb29e59421da4841d08945a548fb41ce
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2026-03-26 11:08:16 +0100

    Implement core application layout, employe sales management, station details, and NGRX effects for clients and pumps.

--- src/app/features/employe/station-details/station-details.ts
+++ src/app/features/employe/station-details/station-details.ts
@@ -6,6 +6,7 @@ import { Station } from '../../../core/models/station';
 import * as StationActions from '../store/station/station.actions';
 import { selectCurrentStation, selectStationLoading, selectStationError } from '../store/station/station.selectors';
 import { LoadingSpinnerComponent } from '../../../shared/components/loading-spinner/loading-spinner.component';
+import { AuthService } from '../../../core/services/Auth';
 @Component({
   selector: 'app-station-details',
   standalone: true,
@@ -17,13 +18,15 @@ export class StationDetails implements OnInit {
   station$: Observable<Station | null>;
   loading$: Observable<boolean>;
   error$: Observable<string | null>;
-  stationId = 2;
-  constructor(private store: Store) {
+  constructor(private store: Store, private authService: AuthService) {
     this.station$ = this.store.select(selectCurrentStation);
     this.loading$ = this.store.select(selectStationLoading);
     this.error$ = this.store.select(selectStationError);
   }
   ngOnInit(): void {
-    this.store.dispatch(StationActions.loadStation({ id: this.stationId }));
+    const stationId = this.authService.getStationId();
+    if (stationId) {
+      this.store.dispatch(StationActions.loadStation({ id: stationId }));
+    }
   }
-}
+}
