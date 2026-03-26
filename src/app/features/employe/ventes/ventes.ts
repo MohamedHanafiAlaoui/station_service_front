30ecdc73a7e538440972d48031fb9d543b6b9888
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2026-03-26 11:17:25 +0100

    Implement core application layout, sidebar navigation, and initial sales management features.

--- src/app/features/employe/ventes/ventes.ts
+++ src/app/features/employe/ventes/ventes.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 @Component({
   selector: 'app-ventes',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './ventes.html',
   styleUrl: './ventes.css',
 })
