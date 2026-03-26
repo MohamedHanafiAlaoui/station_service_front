1e044a06c78d38d3aa2680c138b37f47b3fcf72a
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2026-03-26 11:48:06 +0100

    add sidebar component with navigation links and logout functionality

--- src/app/shared/components/sidebar/sidebar.ts
+++ src/app/shared/components/sidebar/sidebar.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../../core/services/Auth';
+
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
-export class Sidebar {}
+export class Sidebar {
+  private readonly auth = inject(AuthService);
+  private readonly router = inject(Router);
+
+  logout() {
+    this.auth.logout();
+    this.router.navigate(['/auth'], { replaceUrl: true });
+  }
+}
