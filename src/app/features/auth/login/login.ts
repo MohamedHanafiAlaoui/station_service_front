507a209bd45db6b1a49515ce176617547ac1d84c
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2026-03-26 10:51:13 +0100

    feat: Implement comprehensive employe module including dashboard, client, pump, and sales management with core UI components and login.

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../../core/services/Auth';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -12,9 +13,15 @@ import { AuthService } from '../../../core/services/Auth';
 export class Login {
   username = '';
   password = '';
+  oldPassword = '';
+  newPassword = '';
+  confirmPassword = '';
   error = '';
+  success = '';
+  showChangePassword = false;
+  
   USERNAME_REGEX = /^[a-zA-Z0-9._-]{4,20}$/;
-  PASSWORD_REGEX = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$%^&+=!]).{8,}$/;
+  PASSWORD_REGEX = /^[a-zA-Z0-9@#$%^&+=!]{8,30}$/;
   constructor(
     private auth: AuthService,
     private router: Router,
@@ -37,7 +44,57 @@ export class Login {
         else if (role === 'ROLE_CLIENT') this.router.navigate(['/client']);
       },
       error: (msg) => {
-    this.error = typeof msg === 'string' ? msg : 'Erreur de connexion.';      },
+        this.error = typeof msg === 'string' ? msg : 'Erreur de connexion.';
+      },
+    });
+  }
+
+  toggleChangePassword() {
+    this.showChangePassword = !this.showChangePassword;
+    this.error = '';
+    this.success = '';
+  }
+
+  changePassword() {
+    if (!this.username || !this.oldPassword || !this.newPassword || !this.confirmPassword) {
+      this.error = "Veuillez remplir tous les champs.";
+      return;
+    }
+    if (this.newPassword !== this.confirmPassword) {
+      this.error = "Les nouveaux mots de passe ne correspondent pas.";
+      return;
+    }
+    if (!this.PASSWORD_REGEX.test(this.newPassword)) {
+      this.error = "Nouveau mot de passe invalide (8+ caractères).";
+      return;
+    }
+
+    this.error = '';
+    const data = {
+      username: this.username,
+      oldPassword: this.oldPassword,
+      newPassword: this.newPassword
+    };
+
+    this.auth.changePassword(data).subscribe({
+      next: () => {
+        this.success = "Mot de passe changé avec succès. Vous pouvez maintenant vous connecter.";
+        this.showChangePassword = false;
+        this.password = '';
+      },
+      error: (msg) => {
+        this.error = msg;
+      }
+    });
+  }
+
+  onForgotPassword() {
+    Swal.fire({
+      title: 'Mot de passe oublié ?',
+      text: "Veuillez contacter l'administrateur de la station pour réinitialiser vos identifiants.",
+      icon: 'info',
+      confirmButtonText: 'Compris',
+      confirmButtonColor: '#3b82f6'
     });
   }
 }
